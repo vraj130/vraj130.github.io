53e37df50a91303e0a4f9e5ff56039095c1db45f
vraj130 <63923064+vraj130@users.noreply.github.com>
2026-04-14 07:31:41 +0000

    Deploying to gh-pages from @ vraj130/vraj130.github.io@1a8b0abf94018019719f538151e2ead25178df00 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "vraj130/vraj130.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
